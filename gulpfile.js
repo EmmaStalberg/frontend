76ca7608faa255bb2220d3baf9374d819ec1e614
Haoru <haoru9@gmail.com>
2024-11-27 10:22:25 +0000

    Initial commit for Dev-haoru-frontend branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
-import { globIterate } from "glob";
-import { availableParallelism } from "node:os";
+// Import BrowserSync for live reloading and development server
+import browserSync from "browser-sync";
 
-process.env.UV_THREADPOOL_SIZE = availableParallelism();
+// Import Gulp for defining tasks
+import gulp from "gulp";
 
-const gulpImports = [];
+// Create a BrowserSync instance
+const bs = browserSync.create();
 
-for await (const gulpModule of globIterate("build-scripts/gulp/*.?(c|m)js", {
-  dotRelative: true,
-})) {
-  gulpImports.push(import(gulpModule));
-}
+// Task: Initialize the development server
+gulp.task("develop-app", (done) => {
+  // Initialize the server
+  bs.init({
+    server: {
+      baseDir: "./", // Root directory for the server
+      index: "index.html", // Entry file for the server
+    },
+    port: 3000, // Port to listen on
+  });
 
-// Since all tasks are currently registered with gulp.task(), this is enough
-// If any are converted to named exports, need to loop and aggregate exports here
-await Promise.all(gulpImports);
+  // Watch for file changes and reload the browser
+  gulp.watch("src/**/*.*").on("change", bs.reload);
+
+  done();
+});
+
+// Default task (for example, could be used to run 'develop-app')
+gulp.task("default", gulp.series("develop-app"));
